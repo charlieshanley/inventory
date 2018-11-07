a67a104ac0e372c642824dd0ca366e4e07ac6940
Charles Hanley <chanley@mathematica-mpr.com>
2018-11-06 19:31:09 -0500

    minimal working implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,30 +5,24 @@ module Lib ( files ) where
 
 import Turtle
 import Prelude  hiding (FilePath)
-import Data.Csv hiding (Parser)
-import GHC.Generics    (Generic)
 import Data.Maybe      (fromMaybe)
 
 files :: IO ()
 files = do
     (src, dest) <- options "TODO: add description" parser
-    info <- infoIf (\_ -> return True) src 
-    maybe stdout output dest info
+    maybe stdout output dest $ return "Path,Name,Extension,Size" <|> do
+        p <- lsif (\_ -> return True) src 
+        info p
+
+info :: FilePath -> Shell Line
+info p = do
+    size <- du p
+    let ext = fromMaybe "" $ extension p
+        rec = format (fp%","%fp%","%s%","%sz) (dirname p) (basename p) ext size
+    fromMaybe empty (return <$> textToLine rec)
+
 
 parser :: Parser (FilePath, Maybe FilePath)
 parser = (,) <$>           optPath "src"  's' "The source directory"
              <*> optional (optPath "dest" 'd' "The destination csv file")
-
-data Info = Info { path :: FilePath, name :: FilePath, ext :: Text, size :: Size }
-    deriving (Generic, Show)
-
-instance ToNamedRecord Info
-instance ToField FilePath where toField = toField . format fp
-instance ToField Size     where toField = toField . format sz
-
-info :: MonadIO io => FilePath -> io Info
-info p = Info (dirname p) (basename p) (fromMaybe "" (extension p)) <$> du p
-
-infoIf :: (FilePath -> IO Bool) -> FilePath -> Shell Info
-infoIf pred = info <=< lsif pred
 
