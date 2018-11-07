b695fcc30ac9322f8b89b8a00d663bcb3482b3fe
Charles Hanley <chanley@mathematica-mpr.com>
2018-11-07 00:51:38 -0500

    rename to inventory; directory not dirname

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,7 +17,7 @@ inventory = do
         f      <- if isFile then return path else empty
         size   <- du f
         let ext = fromMaybe "" $ extension f
-            rec = format (fp%","%fp%","%s%","%sz) (dirname f) (basename f) ext size
+            rec = format (fp%","%fp%","%s%","%sz) (directory f) (basename f) ext size
         fromMaybe empty (return <$> textToLine rec)
 
 parser :: Parser (FilePath, Maybe FilePath)
