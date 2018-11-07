084bb4c9a40c0fcfd6d30e658f2a2a3391ce10b6
Charles Hanley <chanley@mathematica-mpr.com>
2018-11-06 23:28:18 -0500

    do not traverse hidden dirs; get info only for files; error msg for bad src dir

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,23 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric     #-}
 
-module Lib ( files ) where
+module Lib ( inventory ) where
 
 import Turtle
 import Prelude  hiding (FilePath)
 import Data.Maybe      (fromMaybe)
 
-files :: IO ()
-files = do
+inventory :: IO ()
+inventory = do
     (src, dest) <- options "CSV inventory of files" parser
+    exists <- testdir src
+    when (not exists) $ die (format ("dir does not exist: "%fp) src)
+    let files = do path <- lsif (return . notHidden) src 
+                   isFile <- testfile path
+                   if isFile then return path else empty
     maybe stdout output dest $ return "Path,Name,Extension,Size" <|> do
-        p <- lsif (\_ -> return True) src 
-        size <- du p
-        let ext = fromMaybe "" $ extension p
-            rec = format (fp%","%fp%","%s%","%sz) (dirname p) (basename p) ext size
+        f    <- files
+        size <- du f
+        let ext = fromMaybe "" $ extension f
+            rec = format (fp%","%fp%","%s%","%sz) (dirname f) (basename f) ext size
         fromMaybe empty (return <$> textToLine rec)
 
 parser :: Parser (FilePath, Maybe FilePath)
 parser = (,) <$>           optPath "src"  's' "The source directory"
-             <*> optional (optPath "dest" 'd' "The destination csv file")
+             <*> optional (optPath "dest" 'd' "The destination csv file (stdout if missing)")
 
+notHidden :: FilePath -> Bool
+notHidden = null . match (begins ".") . format fp . basename
